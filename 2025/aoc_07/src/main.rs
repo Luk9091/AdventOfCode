fdae43e362ff0378258175f38d93598334256f7f
Łukasz Przystupa <lukasz9091s@gmail.com>
2026-03-14 20:59:43 +0100

    Day 7 part 1 -- more rusty :D

--- 2025/aoc_07/src/main.rs
+++ 2025/aoc_07/src/main.rs
@@ -1,59 +1,43 @@
-use std::{collections::{BTreeMap, BTreeSet}, fs};
+use std::{collections::{BTreeSet}, fs};
 
 fn load_input(path: &str) -> Vec<String> {
-    let file  = fs::read_to_string(path).expect("File {} don't exists!");
+    let file  = fs::read_to_string(path).expect("File don't exists!");
     file.lines()
         .map(|s| s.to_string())
         // .filter(|s| s.contains("^") || s.contains("S"))
         .collect()
 }
 
-fn split_location(line: &String, split_char: char) -> BTreeSet<usize> {
-    let mut set: BTreeSet<usize> = BTreeSet::new();
-    for (i, c) in line.chars().enumerate() {
-        if c == split_char {
-            // println!("i: {i}, c: {c}");
-            set.insert(i);
-        }
-    }
+fn find_first_element(line: &str) -> BTreeSet<usize>{
+    let mut set = BTreeSet::new();
+    let start_position = line.find("S")
+        .expect("Cannot find first position");
+    set.insert(start_position);
     set
-}
-fn make_neighbour(set: BTreeSet<usize>, old: BTreeSet<usize>) -> BTreeSet<usize> {
-    let mut neighbour = BTreeSet::new();
-    let old = &old - &set;
-    for value in set.iter() {
-        neighbour.insert(value-1);
-        neighbour.insert(value+1);
-    }
-    &neighbour | &old
-}
-
-fn count_split(line: &String, lasers: &BTreeSet<usize>) -> i32{
-    let mut count = 0;
-    for index in lasers.iter() {
-        if line.chars().nth(*index).unwrap() == '^' {
-            count += 1;
-        }
-    }
-    count
 }
 
 fn main() {
     // let lines = load_input("example.txt");
     let lines = load_input("input.txt");
 
-    let start = split_location(&lines[0], 'S');
+    let start = find_first_element(&lines[0]);
+
 
     let last = lines.iter()
-        .fold((start, 0), |(old, sum),  line| {
-            if line.contains("^") /*|| line.contains("S")*/ {
-                let count = count_split(&line, &old) + sum;
-                let neighbour = make_neighbour(split_location(&line, '^'), old);
-                println!("{} | {:?} \t {}", line, neighbour, neighbour.len());
-                (neighbour, count)
-            } else {
-                (old, sum)
+        .skip(1)
+        .fold((start, 0), |(previous_line, sum),  line| {
+            let mut count = 0;
+            let mut next_line = BTreeSet::new();
+            for index in previous_line {
+                if line.as_bytes()[index] == b'^' {
+                    next_line.insert(index-1);
+                    next_line.insert(index+1);
+                    count += 1;
+                } else {
+                    next_line.insert(index);
+                }
             }
+            (next_line, sum + count)
         });
 
     println!("Sum: {}", last.1);
